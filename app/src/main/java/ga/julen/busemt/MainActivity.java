14de2bf79f328b72eb1c78ffc812afe6f49f8a3b
julenb <21701719@live.uem.es>
2019-01-15 17:13:59 +0100

    JSON POST Request

--- app/src/main/java/ga/julen/busemt/MainActivity.java
+++ app/src/main/java/ga/julen/busemt/MainActivity.java
@@ -1,6 +1,7 @@
 package ga.julen.busemt;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -10,6 +11,12 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,8 +24,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private Context context;
     private GoogleMap map;
     private LocationManager locationManager;
 
@@ -26,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -43,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-            map.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 17.0f));
         }
 
         @Override
@@ -69,4 +83,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         map.setMyLocationEnabled(true);
         map.getUiSettings().setMyLocationButtonEnabled(true);
     }
+
+    private ArrayList<MarkerOptions> paradas(Location location) {
+        ArrayList<MarkerOptions> markers=new ArrayList<>();
+        HashMap<String, String> params = new HashMap<>();
+        params.put("idClient", context.getString(R.string.idClient));
+        params.put("passKey", context.getString(R.string.passKey));
+        params.put("Radius", "500");
+        params.put("latitude", String.valueOf(location.getLatitude()));
+        params.put("longitude", String.valueOf(location.getLongitude()));
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                "https://openbus.emtmadrid.es:9443/emt-proxy-server/last/geo/GetStopsFromXY.php",
+                new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+    }
+
 }
